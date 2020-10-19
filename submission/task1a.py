256f5afb3d15074e3b4800675d09afc8503ce0af
valkyriean <aegis_fate@outlook.com>
2020-10-19 14:45:50 +1100

    1

---
+++ submission/task1a.py
@@ -0,0 +1,18 @@
+import csv
+import string
+
+import pandas as pd
+import textdistance
+
+abt = pd.read_csv("abt_small.csv", encoding='ISO-8859-1')
+buy = pd.read_csv("buy_small.csv", encoding='ISO-8859-1')
+task1a = open("task1a.csv", 'w', newline='')
+task1a_writer = csv.writer(task1a)
+task1a_writer.writerow(["idAbt", "idBuy"])
+for abt_i, abt_row in abt.iterrows():
+    abt_name = abt_row["name"].lower().translate(str.maketrans('', '', string.punctuation))
+    for buy_i, buy_row in buy.iterrows():
+        buy_name = buy_row["name"].lower().translate(str.maketrans('', '', string.punctuation))
+        score = textdistance.lcsseq.normalized_similarity(abt_name, buy_name)
+        if score > 0.4:
+            task1a_writer.writerow([abt_row['idABT'], buy_row['idBuy']])
--- submission/task1b.py
+++ submission/task1b.py
@@ -10,13 +10,13 @@ for abt_i, abt_row in abt.iterrows():
     blocks_key = abt_row["name"].lower().split(" ", 1)[0]
     abt_blocks_writer.writerow([blocks_key, abt_row['idABT']])
 
-buy = pd.read_csv("buy_small.csv", encoding='ISO-8859-1')
+buy = pd.read_csv("buy.csv", encoding='ISO-8859-1')
 buy_blocks = open("buy_blocks.csv", 'w', newline='')
 buy_blocks_writer = csv.writer(buy_blocks)
 buy_blocks_writer.writerow(["block_key", "product_id"])
 
 for buy_i, buy_row in buy.iterrows():
-    if len(buy_row["manufacturer"]) > 0:
+    if isinstance(buy_row["manufacturer"], str):
         blocks_key = buy_row["manufacturer"].lower().split(" ", 1)[0]
     else:
         blocks_key = buy_row["name"].lower().split(" ", 1)[0]
