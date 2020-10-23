f819e9c67382989feabfa1c5f8307760ae14843a
valkyriean <aegis_fate@outlook.com>
2020-10-23 18:20:11 +1100

    1

--- task2a.py
+++ task2a.py
@@ -21,30 +21,30 @@ combine.sort_values(by=['Country Name'], ascending=True)
 # Extract data we are interested in
 x = combine[combine.columns[3:23]]
 y = combine[combine.columns[26]]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=200)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, test_size=0.3, random_state=200)
 # impute with median
 x_train = x_train.replace('..', np.NaN)
 x_train = x_train.fillna(x_train.median()).astype("float64")
 x_test = x_test.replace('..', np.NaN)
-x_test = x_test.fillna(x_test.median()).astype("float64")
+x_test = x_test.fillna(x_train.median()).astype("float64")
 # Scale x_train
 for key, value in x_train.iteritems():
     task2a_writer.writerow([key, round(value.median(), 3), round(value.mean(), 3), round(value.var(), 3)])
-    value = preprocessing.scale(value)
-    x_train[key] = value
 # Scale x_test
-for key, value in x_test.iteritems():
-    x_test[key] = preprocessing.scale(value)
+scaler = preprocessing.StandardScaler().fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
 # K Neighbors Classifier with k = 3
-neigh3 = KNeighborsClassifier(n_neighbors=3)
-neigh3.fit(x_train, y_train)
-neigh3_pred = neigh3.predict(x_test)
-neigh3_score = accuracy_score(y_test, neigh3_pred)
+knn3 = KNeighborsClassifier(n_neighbors=3)
+knn3.fit(x_train, y_train)
+knn3_pred = knn3.predict(x_test)
+knn3_score = accuracy_score(y_test, knn3_pred)
 # K Neighbors Classifier with k = 7
-neigh7 = KNeighborsClassifier(n_neighbors=7)
-neigh7.fit(x_train, y_train)
-neigh7_pred = neigh7.predict(x_test)
-neigh7_score = accuracy_score(y_test, neigh7_pred)
+knn7 = KNeighborsClassifier(n_neighbors=7)
+knn7.fit(x_train, y_train)
+knn7_pred = knn7.predict(x_test)
+knn7_score = accuracy_score(y_test, knn7_pred)
 # Decision Tree Classifier
 clf = DecisionTreeClassifier(max_depth=3, random_state=200)
 clf.fit(x_train, y_train)
@@ -52,5 +52,5 @@ clf_pred = clf.predict(x_test)
 clf_score = accuracy_score(y_test, clf_pred)
 
 print("Accuracy of decision tree: " + str(round(clf_score, 3)))
-print("Accuracy of k-nn (k=3): " + str(round(neigh3_score, 3)))
-print("Accuracy of k-nn (k=7): " + str(round(neigh7_score, 3)))
+print("Accuracy of k-nn (k=3): " + str(round(knn3_score, 3)))
+print("Accuracy of k-nn (k=7): " + str(round(knn7_score, 3)))
